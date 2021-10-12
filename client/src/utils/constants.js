010c17caf390fba861d641f46e4d777f22781aeb
juliantijo123 <julian8418@hotmail.com>
2021-10-11 21:46:13 -0500

    Carpeta utils

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://ecommerce-grupo1.herokuapp.com";
+export const API_URL = "https://localhost:1337";
 export const TOKEN = "token";
 export const PRODUCTS = "products";
